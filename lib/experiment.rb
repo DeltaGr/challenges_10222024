8306689f68d6bdfe284e6dff31b634c0afed2ed5
Charlotte Lichter <celichter@gmail.com>
2024-10-22 21:14:15 +0200

    first commit

---
+++ lib/experiment.rb
@@ -0,0 +1,74 @@
+# Warning:
+# - One line of code for each method
+# - Just look in the doc for the right method of the String, Integer, and Array classes!
+
+def get_rid_of_surrounding_whitespaces(a_string)
+  a_string.strip
+  # TODO: return a copy of the string with leading and trailing whitespaces removed
+  # example: get_rid_of_surrounding_whitespaces("  hey yo  ") => "hey yo"
+
+end
+puts get_rid_of_surrounding_whitespaces("  hey yo  ")
+
+
+def belongs_to?(a_string, a_word)
+    a_string.include?(a_word)
+
+  # TODO: return true if a_string contains a_word
+  # example: belongs_to?("hey jude", "jude") => true
+  # SOLUTION:
+  # Probtip: try to read the code backwards
+  # For example: Question: We want to know, if a_word is included in a_string
+  # afterwards needs a puts command with the actual phrase which should be checked
+  # than check if it is running in the terminal by using "ruby lib/experiment.rb"
+  # Remember: It is a different folder for different challenges
+end
+puts belongs_to?("hey jude", "jude")
+
+
+def replace(initial_string, old_letter, new_letter)
+  # TODO: return a copy of the string with the new letter replacing the old one
+  # example: replace("casanova", "a", "o") => "cosonovo"
+  # my_string = old_letter_to_new_letter
+  #   replace_old_letter(:leet_speak) do
+  #     self.gsub("casanova","a", "o")
+  #     return
+  #   end
+  # end
+
+  # s = "casanova".leet_speak()
+
+  # puts s
+"casanova".gsub(/[aeiau]/, {"a" => "o"})
+# "cosonovo"
+end
+
+
+def exactly_divide(an_integer, a_divider)
+  # TODO: return division of an_integer by a_divider (computing the floating division, NOT the euclidean one)
+  # example: exactly_divide(13, 4) => 3.25
+  13.fdiv(4)
+ end
+
+  puts exactly_divide(13, 4)
+
+def divisible_by_two?(an_integer)
+  # TODO: return true if an_integer is divisible by 2
+  # example: divisible_by_two?(6) => true
+end
+
+def random_subset(an_array, sample_size)
+  # TODO: return a random subset of an_array with size sample_size
+  # example: random_subset(("a".."z").to_a, 4) => ["u", "q", "l", "t"]
+end
+
+def randomize(an_array)
+  # TODO: return a randomized copy of an_array
+  # example: randomize([1, 2, 3, 4]) => [2, 1, 4, 3]
+end
+
+def ascending_order(an_array)
+  # TODO: return a copy of an_array with elements in ascending order
+  # example: ascending_order([7, 3, 1, 6, 9]) => [1, 3, 6, 7, 9]
+end
+git push
